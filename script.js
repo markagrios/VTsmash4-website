b33f9866fbd1a667336b00484458feedcf9add73
Mark Agrios <magrios@gmail.com>
2017-05-22 00:14:02 -0400

    sliding thing

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("smaesh")
+
+$(document).click(function() {
+  $("#side-bar").toggle( "slide" );
+  $("#main").css("width","100%");
+});
+
+
+//
+// $("#sidebar-x").hover(
+// 	function() {
+//     //$("#side-bar").addClass("0width");
+//     console.log("clicked")
+//   } ,20
+// );
